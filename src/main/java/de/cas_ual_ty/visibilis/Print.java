4949cb053c04ed5a914db8bff3dce7fd11b19e2e
CAS-ual-TY-Laptop <cas_ual_ty@gmx.de>
2019-08-15 17:34:02 +0200

    Added documentation for Print class

--- src/main/java/de/cas_ual_ty/visibilis/Print.java
+++ src/main/java/de/cas_ual_ty/visibilis/Print.java
@@ -4,12 +4,20 @@ import java.util.ArrayList;
 
 public class Print
 {
+	/**
+	 * All nodes in this print.
+	 */
 	protected final ArrayList<Node> nodes = new ArrayList<Node>();
 	
 	public Print()
 	{
 	}
 	
+	/**
+	 * Add a node to this print. It will be added on top.
+	 * @param node The node to be added.
+	 * @return The print instance itself.
+	 */
 	public Print addNode(Node node)
 	{
 		if(this.containsNode(node))
@@ -21,17 +29,31 @@ public class Print
 		return this;
 	}
 	
+	/**
+	 * Remove a node from the print and disconnect it from all nodes. Used to delete a node safely.
+	 * @param node The node to be removed and disconnected.
+	 * @return <b>true</b> if this print contained the given node.
+	 */
 	public boolean removeNode(Node node)
 	{
 		node.disconnect();
 		return this.nodes.remove(node);
 	}
 	
+	/**
+	 * Remove a node from the print but keep the connections. Used to reposition the node in the node list.
+	 * @param node The node to be removed.
+	 * @return <b>true</b> if this print contained the given node.
+	 */
 	public boolean removeNodeKeepConnections(Node node)
 	{
 		return this.nodes.remove(node);
 	}
 	
+	/**
+	 * @param node The node to check for.
+	 * @return <b>true</b> if the given node is part of this print.
+	 */
 	public boolean containsNode(Node node)
 	{
 		return this.nodes.contains(node);
@@ -53,9 +75,9 @@ public class Print
 	}
 	
 	/**
-	 * Recursive method. Executes all connected exec nodes in succession to the given parameter
-	 * @param node The node to start the exec chain from
-	 * @return <b>true</b> if the given parameter exec node and all child exec nodes could be calculated successfully
+	 * Recursive method. Executes all connected exec nodes in succession to the given parameter.
+	 * @param node The node to start the exec chain from.
+	 * @return <b>true</b> if the given parameter exec node and all child exec nodes could be calculated successfully.
 	 */
 	protected boolean exec(NodeExec node)
 	{
