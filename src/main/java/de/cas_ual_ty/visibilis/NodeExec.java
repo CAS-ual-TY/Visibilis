11352cb0ed8f68f85a48c6f9ab0b23a6b696f7e1
CAS-ual-TY-Laptop <cas_ual_ty@gmx.de>
2019-08-15 17:56:38 +0200

    Added further documentation to the NodeExec class

--- src/main/java/de/cas_ual_ty/visibilis/NodeExec.java
+++ src/main/java/de/cas_ual_ty/visibilis/NodeExec.java
@@ -10,8 +10,10 @@ public abstract class NodeExec extends Node
 	}
 	
 	/**
-	 * After calculation, get the next output of the execution data type which basically just decides the next node to calculate. The next node's non calculated parents are calculated before.
-	 * @return The next node in execution order.
+	 * After calculation, get the next output of the exec data type (basically just the next exec node to calculate). This is getting called repeatedly until <b>null</b> is returned.
+	 * @see de.cas_ual_ty.visibilis.Print#execute()
+	 * @param index The index of the sub nodes starting at 0, incremented after every call.
+	 * @return The next node in execution order. <b>null</b> if there are no more sub nodes.
 	 */
 	@Nullable
 	public abstract Output getOutExec(int index);
