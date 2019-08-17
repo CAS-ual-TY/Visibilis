1aff594a65ba846924265b9e90bc7997dc0f731e
CAS-ual-TY-Laptop <cas_ual_ty@gmx.de>
2019-08-17 15:55:20 +0200

    Added init methods to the Visibilis main mod class
    preInit, init, postInit

--- src/main/java/de/cas_ual_ty/visibilis/Visibilis.java
+++ src/main/java/de/cas_ual_ty/visibilis/Visibilis.java
@@ -1,6 +1,10 @@
 package de.cas_ual_ty.visibilis;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = "")
 public class Visibilis
@@ -8,4 +12,22 @@ public class Visibilis
 	public static final String MOD_ID = "visibilis";
 	public static final String MOD_NAME = "Visibilis";
 	public static final String MOD_VERSION = "1.0.0.0";
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		
+	}
 }
