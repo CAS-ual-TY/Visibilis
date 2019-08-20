a73717f86cea0482f2a2e2bd4765a6134dd69b83
CAS-ual-TY-Laptop <cas_ual_ty@gmx.de>
2019-08-20 15:21:33 +0200

    Added constant SQRT_2 node

---
+++ src/main/java/de/cas_ual_ty/visibilis/node/constant/NodeSQRT.java
@@ -0,0 +1,24 @@
+package de.cas_ual_ty.visibilis.node.constant;
+
+import de.cas_ual_ty.visibilis.node.general.NodeFloatConst;
+import net.minecraft.util.math.MathHelper;
+
+public class NodeSQRT extends NodeFloatConst
+{
+    public NodeSQRT()
+    {
+        super();
+    }
+    
+    @Override
+    protected float getValue()
+    {
+        return MathHelper.SQRT_2;
+    }
+    
+    @Override
+    public String getID()
+    {
+        return "pi";
+    }
+}
