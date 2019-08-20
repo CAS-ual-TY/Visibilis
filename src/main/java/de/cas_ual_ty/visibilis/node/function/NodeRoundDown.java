96f487dafc4f26c46bc18545916b5c12a32b2ade
CAS-ual-TY-Laptop <cas_ual_ty@gmx.de>
2019-08-20 14:54:16 +0200

    Added rounding nodes

---
+++ src/main/java/de/cas_ual_ty/visibilis/node/function/NodeRoundDown.java
@@ -0,0 +1,23 @@
+package de.cas_ual_ty.visibilis.node.function;
+
+import de.cas_ual_ty.visibilis.node.general.NodeNumber1to1;
+
+public class NodeRoundDown extends NodeNumber1to1
+{
+    @Override
+    protected Number calculate(Number in1)
+    {
+        if (in1 instanceof Integer)
+        {
+            return in1.intValue(); // Dont round if already int
+        }
+        
+        return Math.floor(in1.floatValue());
+    }
+    
+    @Override
+    public String getID()
+    {
+        return "round_down";
+    }
+}
