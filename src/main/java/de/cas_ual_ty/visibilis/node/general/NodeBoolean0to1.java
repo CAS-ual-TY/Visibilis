a02fd7946704bfa20b10e5ddc792d13cd4efc1d0
CAS-ual-TY-Laptop <cas_ual_ty@gmx.de>
2019-08-22 13:22:44 +0200

    Add top prev commit
    Forgot to check this

---
+++ src/main/java/de/cas_ual_ty/visibilis/node/general/NodeBoolean0to1.java
@@ -0,0 +1,46 @@
+package de.cas_ual_ty.visibilis.node.general;
+
+import de.cas_ual_ty.visibilis.datatype.DataType;
+import de.cas_ual_ty.visibilis.node.Node;
+import de.cas_ual_ty.visibilis.node.Output;
+
+public abstract class NodeBoolean0to1 extends Node
+{
+    public final Output<Boolean> out1;
+    
+    public boolean value;
+    
+    public NodeBoolean0to1(int outputAmt, int inputAmt)
+    {
+        super(outputAmt, inputAmt);
+        this.out1 = new Output<Boolean>(0, this, DataType.BOOLEAN, "out1");
+    }
+    
+    public NodeBoolean0to1()
+    {
+        this(1, 0);
+    }
+    
+    @Override
+    public boolean doCalculate()
+    {
+        this.value = this.getValue();
+        return true;
+    }
+    
+    /**
+     * @return The static value.
+     */
+    protected abstract boolean getValue();
+    
+    @Override
+    public <B> B getOutputValue(int index)
+    {
+        if (index == this.out1.id)
+        {
+            return (B) (Boolean) this.value;
+        }
+        
+        return null;
+    }
+}
