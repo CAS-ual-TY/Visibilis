489eeb41661a2dabf6646673e210097354e0e42e
CAS-ual-TY-Laptop <cas_ual_ty@gmx.de>
2019-08-22 18:41:28 +0200

    Registered the rest of the nodes

--- src/main/java/de/cas_ual_ty/visibilis/Visibilis.java
+++ src/main/java/de/cas_ual_ty/visibilis/Visibilis.java
@@ -2,6 +2,27 @@ package de.cas_ual_ty.visibilis;
 
 import de.cas_ual_ty.visibilis.handler.VEventHandler;
 import de.cas_ual_ty.visibilis.handler.VGuiHandler;
+import de.cas_ual_ty.visibilis.node.calculate.NodeAddition;
+import de.cas_ual_ty.visibilis.node.calculate.NodeDivision;
+import de.cas_ual_ty.visibilis.node.calculate.NodeExponentiation;
+import de.cas_ual_ty.visibilis.node.calculate.NodeLogarithm10;
+import de.cas_ual_ty.visibilis.node.calculate.NodeLogarithm1p;
+import de.cas_ual_ty.visibilis.node.calculate.NodeLogarithmE;
+import de.cas_ual_ty.visibilis.node.calculate.NodeModulo;
+import de.cas_ual_ty.visibilis.node.calculate.NodeMultiplication;
+import de.cas_ual_ty.visibilis.node.calculate.NodeRoot;
+import de.cas_ual_ty.visibilis.node.calculate.NodeSubtraction;
+import de.cas_ual_ty.visibilis.node.constant.NodeE;
+import de.cas_ual_ty.visibilis.node.constant.NodeFalse;
+import de.cas_ual_ty.visibilis.node.constant.NodePi;
+import de.cas_ual_ty.visibilis.node.constant.NodeSQRT2;
+import de.cas_ual_ty.visibilis.node.constant.NodeTrue;
+import de.cas_ual_ty.visibilis.node.function.NodeCosines;
+import de.cas_ual_ty.visibilis.node.function.NodeRound;
+import de.cas_ual_ty.visibilis.node.function.NodeRoundDown;
+import de.cas_ual_ty.visibilis.node.function.NodeRoundUp;
+import de.cas_ual_ty.visibilis.node.function.NodeSines;
+import de.cas_ual_ty.visibilis.node.function.NodeTangent;
 import de.cas_ual_ty.visibilis.node.logic.NodeAND;
 import de.cas_ual_ty.visibilis.node.logic.NodeNAND;
 import de.cas_ual_ty.visibilis.node.logic.NodeNOR;
@@ -63,6 +84,41 @@ public class Visibilis
         Visibilis.channel = NetworkRegistry.INSTANCE.newSimpleChannel(Visibilis.MOD_ID);
         Visibilis.channel.registerMessage(MessageHandlerItem.class, MessageItem.class, 0, Side.SERVER);
         
+        this.registerNodes();
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        
+    }
+    
+    public void registerNodes()
+    {
+        VRegistry.INSTANCE.registerNode(NodeAddition.class, Visibilis.MOD_ID, "addition");
+        VRegistry.INSTANCE.registerNode(NodeDivision.class, Visibilis.MOD_ID, "division");
+        VRegistry.INSTANCE.registerNode(NodeExponentiation.class, Visibilis.MOD_ID, "exponentiation");
+        VRegistry.INSTANCE.registerNode(NodeLogarithm10.class, Visibilis.MOD_ID, "logarithm_10");
+        VRegistry.INSTANCE.registerNode(NodeLogarithm1p.class, Visibilis.MOD_ID, "logarithm_1p");
+        VRegistry.INSTANCE.registerNode(NodeLogarithmE.class, Visibilis.MOD_ID, "logarithm_e");
+        VRegistry.INSTANCE.registerNode(NodeModulo.class, Visibilis.MOD_ID, "modulo");
+        VRegistry.INSTANCE.registerNode(NodeMultiplication.class, Visibilis.MOD_ID, "multiplication");
+        VRegistry.INSTANCE.registerNode(NodeRoot.class, Visibilis.MOD_ID, "root");
+        VRegistry.INSTANCE.registerNode(NodeSubtraction.class, Visibilis.MOD_ID, "subtraction");
+        
+        VRegistry.INSTANCE.registerNode(NodeE.class, Visibilis.MOD_ID, "e");
+        VRegistry.INSTANCE.registerNode(NodeFalse.class, Visibilis.MOD_ID, "false");
+        VRegistry.INSTANCE.registerNode(NodePi.class, Visibilis.MOD_ID, "pi");
+        VRegistry.INSTANCE.registerNode(NodeSQRT2.class, Visibilis.MOD_ID, "sqrt2");
+        VRegistry.INSTANCE.registerNode(NodeTrue.class, Visibilis.MOD_ID, "true");
+        
+        VRegistry.INSTANCE.registerNode(NodeCosines.class, Visibilis.MOD_ID, "cosines");
+        VRegistry.INSTANCE.registerNode(NodeRound.class, Visibilis.MOD_ID, "round");
+        VRegistry.INSTANCE.registerNode(NodeRoundDown.class, Visibilis.MOD_ID, "round_down");
+        VRegistry.INSTANCE.registerNode(NodeRoundUp.class, Visibilis.MOD_ID, "round_up");
+        VRegistry.INSTANCE.registerNode(NodeSines.class, Visibilis.MOD_ID, "sines");
+        VRegistry.INSTANCE.registerNode(NodeTangent.class, Visibilis.MOD_ID, "tangent");
+        
         VRegistry.INSTANCE.registerNode(NodeAND.class, Visibilis.MOD_ID, "and");
         VRegistry.INSTANCE.registerNode(NodeNAND.class, Visibilis.MOD_ID, "nand");
         VRegistry.INSTANCE.registerNode(NodeNOR.class, Visibilis.MOD_ID, "nor");
@@ -70,12 +126,6 @@ public class Visibilis
         VRegistry.INSTANCE.registerNode(NodeOR.class, Visibilis.MOD_ID, "or");
         VRegistry.INSTANCE.registerNode(NodeXNOR.class, Visibilis.MOD_ID, "xnor");
         VRegistry.INSTANCE.registerNode(NodeXOR.class, Visibilis.MOD_ID, "xor");
-    }
-    
-    @EventHandler
-    public void postInit(FMLPostInitializationEvent event)
-    {
-        
     }
     
     // TODO low: Some nice logging here please
