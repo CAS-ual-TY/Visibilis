d2a1848f3180f152e9e078f79b36fea17206cb0a
CAS-ual-TY-Laptop <cas_ual_ty@gmx.de>
2019-08-24 22:38:31 +0200

    Added mixColors method to RenderUtility

--- src/main/java/de/cas_ual_ty/visibilis/util/RenderUtility.java
+++ src/main/java/de/cas_ual_ty/visibilis/util/RenderUtility.java
@@ -652,4 +652,13 @@ public class RenderUtility
             return new Rectangle(l, r, t, b, r - l, b - t);
         }
     }
+    
+    public static float[] mixColors(float[] color1, float[] color2)
+    {
+        float r = (color1[0] + color2[0]) * 0.5F;
+        float g = (color1[1] + color2[1]) * 0.5F;
+        float b = (color1[2] + color2[2]) * 0.5F;
+        
+        return new float[] {r, g, b};
+    }
 }
