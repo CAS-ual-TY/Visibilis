aeaedb92c7012782baf8d113423423c6faaefc9f
CAS-ual-TY-Laptop <cas_ual_ty@gmx.de>
2019-08-25 17:27:26 +0200

    Created test node outputting debug messages

---
+++ src/main/java/de/cas_ual_ty/visibilis/test/VNodeTest.java
@@ -0,0 +1,71 @@
+package de.cas_ual_ty.visibilis.test;
+
+import de.cas_ual_ty.visibilis.Visibilis;
+import de.cas_ual_ty.visibilis.datatype.DataType;
+import de.cas_ual_ty.visibilis.node.Input;
+import de.cas_ual_ty.visibilis.node.NodeExec;
+import de.cas_ual_ty.visibilis.node.Output;
+
+public class VNodeTest extends NodeExec
+{
+    public Input in0;
+    public Input<Boolean> in1;
+    public Input<Number> in2;
+    
+    public VNodeTest()
+    {
+        super(0, 3);
+        this.in0 = new Input(0, this, DataType.EXEC, "test");
+        this.in1 = new Input<Boolean>(1, this, DataType.BOOLEAN, "test");
+        this.in2 = new Input<Number>(2, this, DataType.NUMBER, "test");
+    }
+    
+    @Override
+    public Output getOutExec(int index)
+    {
+        return null;
+    }
+    
+    @Override
+    public boolean doCalculate()
+    {
+        Visibilis.debug("## " + this.getClass().getSimpleName() + " START");
+        
+        this.debugMessage(this.in1);
+        this.debugMessage(this.in2);
+        
+        Visibilis.debug("## " + this.getClass().getSimpleName() + " END");
+        
+        return true;
+    }
+    
+    public void debugMessage(Input in)
+    {
+        if (in.hasConnections())
+        {
+            Visibilis.debug(in.id + ": " + this.in1.getValue());
+        }
+        else
+        {
+            Visibilis.debug(in.id + ": " + "404 NO CONNECTIONS");
+        }
+    }
+    
+    @Override
+    public <B> B getOutputValue(int index)
+    {
+        return null;
+    }
+    
+    @Override
+    public String getID()
+    {
+        return "test";
+    }
+    
+    @Override
+    public String getUnlocalizedName()
+    {
+        return "test";
+    }
+}
