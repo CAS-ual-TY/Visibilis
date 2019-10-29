72565d4b8c3bd1b56d2c45dac2778bafcafc2c7a
CAS-ual-TY-Laptop <cas_ual_ty@gmx.de>
2019-10-29 13:14:01 +0100

    NodeNOT: Changed to extend existing NodeBoolean1to1 class

--- src/main/java/de/cas_ual_ty/visibilis/node/logic/NodeNOT.java
+++ src/main/java/de/cas_ual_ty/visibilis/node/logic/NodeNOT.java
@@ -1,58 +1,18 @@
 package de.cas_ual_ty.visibilis.node.logic;
 
-import de.cas_ual_ty.visibilis.datatype.DataType;
-import de.cas_ual_ty.visibilis.node.ExecProvider;
-import de.cas_ual_ty.visibilis.node.Input;
-import de.cas_ual_ty.visibilis.node.Node;
-import de.cas_ual_ty.visibilis.node.Output;
+import de.cas_ual_ty.visibilis.node.general.NodeBoolean1to1;
 
-public class NodeNOT extends Node
+public class NodeNOT extends NodeBoolean1to1
 {
-    public final Output<Boolean> out1;
-    public final Input<Boolean> in1;
-    
-    public boolean value;
-    
-    public NodeNOT(int outputAmt, int inputAmt)
-    {
-        super(outputAmt, inputAmt);
-        this.out1 = new Output<Boolean>(this, DataType.BOOLEAN, "out1");
-        this.in1 = new Input<Boolean>(this, DataType.BOOLEAN, "in1");
-    }
-    
-    public NodeNOT()
-    {
-        this(1, 1);
-    }
-    
     @Override
-    public boolean doCalculate(ExecProvider provider)
+    protected boolean calculate(boolean in1)
     {
-        this.value = !this.in1.getValue();
-        
-        return true;
-    }
-    
-    @Override
-    public <B> B getOutputValue(int index)
-    {
-        if (index == this.out1.getId())
-        {
-            return (B) (Boolean) this.value;
-        }
-        
-        return null;
+        return !in1;
     }
     
     @Override
     public String getID()
     {
         return "not";
-    }
-    
-    @Override
-    public float[] getColor()
-    {
-        return DataType.BOOLEAN.getColor();
     }
 }
