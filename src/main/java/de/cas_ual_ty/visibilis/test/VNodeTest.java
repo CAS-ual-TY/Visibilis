72e5d04910ef5e211634e17c12139a89b851dfc7
CAS-ual-TY-Laptop <cas_ual_ty@gmx.de>
2019-11-29 17:33:41 +0100

    VNodeTest: Added out exec so you can use it multiple times

--- src/main/java/de/cas_ual_ty/visibilis/test/VNodeTest.java
+++ src/main/java/de/cas_ual_ty/visibilis/test/VNodeTest.java
@@ -7,9 +7,11 @@ import de.cas_ual_ty.visibilis.node.INodeExec;
 import de.cas_ual_ty.visibilis.node.Node;
 import de.cas_ual_ty.visibilis.node.field.Input;
 import de.cas_ual_ty.visibilis.node.field.Output;
+import net.minecraft.util.text.StringTextComponent;
 
 public class VNodeTest extends Node implements INodeExec
 {
+    public Output out1;
     public Input in1;
     public Input<Boolean> in2;
     public Input<Number> in3;
@@ -17,6 +19,7 @@ public class VNodeTest extends Node implements INodeExec
     public VNodeTest()
     {
         super();
+        this.out1 = new Output(this, DataType.EXEC, "out1");
         this.in1 = new Input(this, DataType.EXEC, "in1");
         this.in2 = new Input<>(this, DataType.BOOLEAN, "in2");
         this.in3 = new Input<>(this, DataType.NUMBER, "in3");
@@ -25,7 +28,7 @@ public class VNodeTest extends Node implements INodeExec
     @Override
     public Output getOutExec(int index)
     {
-        return null;
+        return index == 0 ? this.out1 : null;
     }
     
     @Override
@@ -40,18 +43,19 @@ public class VNodeTest extends Node implements INodeExec
     {
         Visibilis.debug("## " + this.getClass().getSimpleName() + " START");
         
-        this.debugMessage(this.in2);
-        this.debugMessage(this.in3);
+        this.debugMessage(provider, this.in2);
+        this.debugMessage(provider, this.in3);
         
         Visibilis.debug("## " + this.getClass().getSimpleName() + " END");
         
         return true;
     }
     
-    public void debugMessage(Input in)
+    public void debugMessage(ExecProvider provider, Input in)
     {
         if (in.hasConnections() || in.hasDisplayValue())
         {
+            provider.getCommandSender().sendFeedback(new StringTextComponent(in.dataType.valueToString(in.getValue())), true);
             Visibilis.debug(in.getId() + ": " + in.getValue());
         }
         else
