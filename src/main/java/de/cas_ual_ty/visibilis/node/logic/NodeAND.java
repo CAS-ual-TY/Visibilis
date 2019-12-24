ffe6cbc650072bffdf54aa5d427ce26815da4e3f
CAS-ual-TY-Laptop <cas_ual_ty@gmx.de>
2019-12-24 13:25:34 +0100

    Updated AND & OR nodes to be XP2 instead of only P2

--- src/main/java/de/cas_ual_ty/visibilis/node/logic/NodeAND.java
+++ src/main/java/de/cas_ual_ty/visibilis/node/logic/NodeAND.java
@@ -1,8 +1,8 @@
 package de.cas_ual_ty.visibilis.node.logic;
 
-import de.cas_ual_ty.visibilis.node.base.dtboolean.NodeBooleanP2;
+import de.cas_ual_ty.visibilis.node.base.dtboolean.NodeBooleanXP2;
 
-public class NodeAND extends NodeBooleanP2
+public class NodeAND extends NodeBooleanXP2
 {
     public NodeAND()
     {
@@ -13,6 +13,20 @@ public class NodeAND extends NodeBooleanP2
     protected Boolean calculate(Boolean in1, Boolean in2)
     {
         return in1 && in2;
+    }
+    
+    @Override
+    protected Boolean calculate(Boolean[] inputs)
+    {
+        for(Boolean input : inputs)
+        {
+            if(!input.booleanValue())
+            {
+                return false;
+            }
+        }
+        
+        return true;
     }
     
     @Override
