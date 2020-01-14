31134bcd5fad77db9298778d073590286e97b5ee
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2020-01-14 12:28:24 +0000

    Removed all magica mundi stuff, updated build.gradle

--- src/main/java/de/cas_ual_ty/magicamundi/visibilis/MMDataType.java
+++
@@ -1,9 +0,0 @@
-package de.cas_ual_ty.magicamundi.visibilis;
-
-import de.cas_ual_ty.magicamundi.visibilis.target.TargetsList;
-import de.cas_ual_ty.visibilis.datatype.DataType;
-
-public class MMDataType
-{
-    public static final DataType<TargetsList> TARGETS_LIST = new DataType<>("targets_list", new float[] { 1F, 0.5F, 1F });
-}
--- src/main/java/de/cas_ual_ty/magicamundi/visibilis/target/Target.java
+++
@@ -1,6 +0,0 @@
-package de.cas_ual_ty.magicamundi.visibilis.target;
-
-public class Target
-{
-    
-}
