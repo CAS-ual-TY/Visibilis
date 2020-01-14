9b2eabbfd965f1e5d597f8333ecfb74a52ffe858
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2020-01-14 10:27:28 +0000

    Outsourced utility class to itself

--- src/main/java/de/cas_ual_ty/visibilis/util/VUtility.java
+++ src/main/java/de/cas_ual_ty/visibilis/util/VUtility.java
@@ -5,20 +5,18 @@ import java.util.ArrayList;
 public class VUtility
 {
     @SuppressWarnings("unchecked")
-    public static <A> A[] createGenericArray(int length)
-    {
-        return (A[])new Object[length];
-    }
-    
-    @SuppressWarnings("unchecked")
     public static <A, B> A cast(B b)
     {
         return (A)b;
     }
     
-    @SuppressWarnings("unchecked")
+    public static <A> A[] createGenericArray(int length)
+    {
+        return VUtility.cast(new Object[length]);
+    }
+    
     public static <A> ArrayList<A> cloneArrayList(ArrayList<A> list)
     {
-        return (ArrayList<A>)list.clone();
+        return VUtility.cast(list.clone());
     }
 }
