7ee42d13ccb937af9a465d5002382ba3b758e854
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2020-01-28 14:14:24 +0100

    Added method to proxy to safely do something with the client player via lambda

--- src/main/java/de/cas_ual_ty/visibilis/proxy/IVSidedProxy.java
+++ src/main/java/de/cas_ual_ty/visibilis/proxy/IVSidedProxy.java
@@ -1,10 +1,17 @@
 package de.cas_ual_ty.visibilis.proxy;
 
+import java.util.function.Consumer;
+
 import de.cas_ual_ty.visibilis.print.PrintProvider;
+import net.minecraft.entity.player.PlayerEntity;
 
 public interface IVSidedProxy
 {
     public default void openGuiPrint(PrintProvider helper)
+    {
+    }
+    
+    public default void doForClientPlayer(Consumer<PlayerEntity> consumer)
     {
     }
 }
--- src/main/java/de/cas_ual_ty/visibilis/proxy/VProxyClient.java
+++ src/main/java/de/cas_ual_ty/visibilis/proxy/VProxyClient.java
@@ -1,8 +1,11 @@
 package de.cas_ual_ty.visibilis.proxy;
 
+import java.util.function.Consumer;
+
 import de.cas_ual_ty.visibilis.print.GuiPrint;
 import de.cas_ual_ty.visibilis.print.PrintProvider;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.text.StringTextComponent;
 
 public class VProxyClient implements IVSidedProxy
@@ -12,5 +15,15 @@ public class VProxyClient implements IVSidedProxy
     {
         //        Minecraft.getMinecraft().displayGuiScreen(new GuiPrintOld(helper));
         Minecraft.getInstance().displayGuiScreen(new GuiPrint(new StringTextComponent("Print"), helper));
+    }
+    
+    @Override
+    public void doForClientPlayer(Consumer<PlayerEntity> consumer)
+    {
+        PlayerEntity p = Minecraft.getInstance().player;
+        if(p != null)
+        {
+            consumer.accept(Minecraft.getInstance().player);
+        }
     }
 }
