803305ff95bb4f6c5c1bdf271d2070c164fd4599
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2020-02-26 21:57:17 +0100

    Created new PrintProviderCapability class (PrintProviderItem extends it)

--- src/main/java/de/cas_ual_ty/visibilis/print/item/PrintProviderItem.java
+++ src/main/java/de/cas_ual_ty/visibilis/print/item/PrintProviderItem.java
@@ -1,39 +1,24 @@
 package de.cas_ual_ty.visibilis.print.item;
 
 import de.cas_ual_ty.visibilis.Visibilis;
-import de.cas_ual_ty.visibilis.print.capability.CapabilityProviderPrint;
 import de.cas_ual_ty.visibilis.print.provider.NodeListProvider;
-import de.cas_ual_ty.visibilis.print.provider.PrintProvider;
-import de.cas_ual_ty.visibilis.util.VNBTUtility;
+import de.cas_ual_ty.visibilis.print.provider.PrintProviderCapability;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 
-public class PrintProviderItem extends PrintProvider
+public class PrintProviderItem extends PrintProviderCapability
 {
     public ItemStack itemStack;
     public int slot;
     
     public PrintProviderItem(NodeListProvider nodeListProvider, ItemStack itemStack, int slot)
     {
-        super(nodeListProvider);
+        super(nodeListProvider, itemStack);
         this.itemStack = itemStack;
         this.slot = slot;
     }
     
     @Override
-    public void init()
-    {
-        this.undoList.setFirst(this.itemStack.getCapability(CapabilityProviderPrint.CAPABILITY_PRINT).orElseThrow(() -> new IllegalArgumentException("LazyOptional must not be empty (4)!")));
-        super.init();
-    }
-    
-    @Override
-    public void onGuiClose()
-    {
-        this.synchToServer(VNBTUtility.savePrintToNBT(this.getPrint()));
-        super.onGuiClose();
-    }
-    
     public void synchToServer(CompoundNBT nbt)
     {
         Visibilis.channel.sendToServer(new MessageItem(this.slot, this.itemStack));
